04bc6ec4c18c1f2dd529c6a2bcb9b94045719d12
Albert Ostaszewski <ostaszewski.albert@gmail.com>
2020-11-05 16:24:20 +0100

    start

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,52 @@
 package com.company;
 
+import com.company.sorting.BubbleSort;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/*
+bubble
+
+selection
+
+gnome
+
+quick
+
+insertion
+
+counting
+
+merge
+
+tree
+
+bucket
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("siema");
-        //asdfasfdgadsgf
+
+        Random r = new Random();
+
+        List<Integer> array = new ArrayList();
+        int lenght = 10;
+        int max = 20;
+
+        for (int i = 0; i < lenght; i++){
+            array.add(r.nextInt(max));
+        }
+
+        System.out.println(array);
+        System.out.println(array.get(3));
+        array.set(3, 40);
+        System.out.println(array);
+
+
+
+
     }
 }
